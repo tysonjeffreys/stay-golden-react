246a624d30bbffa0201e82f1874f84d5d2beeb1d
Tyson <tysonjeffreys@gmail.com>
2021-10-31 19:57:19 -0700

    ImageList and Layout comp

--- src/app/components/ImageList.js
+++ src/app/components/ImageList.js
@@ -1,22 +1,23 @@
-export const ImageList = ({images, subtitle}) => {
+export const ImageList = ({images, subtitle, handleDelete}) => {
 
     return ( 
         <div>
             {images.map((image) => (
-            <div key={image.id}>
-            <div className="row-2">
-                <div className="left-column">
-                    <div className="bicycle-map">
-                        <img src="bicycle-pin-svgrepo-com.svg" />
+                <div key={image.id}>
+                <div className="row-2">
+                    <div className="left-column">
+                        <div className="bicycle-map">
+                            <img src="bicycle-pin-svgrepo-com.svg" />
+                        </div>
+                        <div className="title-column">
+                            <p className="title">{image.title}</p>
+                            <p className="date">{image.date}</p>
+                        </div>
                     </div>
-                    <div className="title-column">
-                        <p className="title">{image.title}</p>
-                        <p className="date">{image.date}</p>
-                    </div>
+                    <div className="home-image">{image.image}</div>
                 </div>
-                <div className="home-image">{image.image}</div>
-            </div>
-            </div>
+                <button onClick={() => handleDelete(image.id)}>delete image</button>
+                </div>
             ))}     
         </div>
     );
--- src/app/components/Layout.js
+++ src/app/components/Layout.js
@@ -1,6 +1,6 @@
 import { Nav } from './Nav'
 import { ImageList } from './ImageList'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export const Layout = () => {
@@ -13,12 +13,17 @@ export const Layout = () => {
 
     const handleDelete = (id) => {
         const newImages = images.filter(image => image.id !== id)
+        setImages(newImages)
     }
+
+    useEffect(() => {
+        
+    })
 
     return (
         <div className="container">
         <Nav />
-        <ImageList images={images} subtitle="All Images" handleDelete={handleDelete}/>
+        <ImageList images={images} subtitle="All Images" handleDelete={handleDelete} />
         </div>
     
     )
